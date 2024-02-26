db2604299b1323724b6c40f48ee2382f983c4956
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-02-26 06:22:21 +0000

    datatypes

---
+++ 01_basics.js/01_variabls.js
@@ -0,0 +1,17 @@
+var accountId = abhay_123;
+let accountNo = 1234567;
+const accountCity = Lucknow;
+accountState = UP ;
+
+var accountId = ab_123;
+let accountNo = 1234;
+const accountCity = now;
+accountState = mp ;
+
+/*prefer not to use var
+because var is use for block scope and funtional work
+*/
+
+
+
+console.table([accountId, accountNo, accountCity, accountState]);
---
+++ 01_basics.js/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" //treat all js code as newer version // but this time is not to use
+
+// DATATYPES
+
+/*
+Number = 2,3,4,5,6
+boolean = True/False
+string = ""
+null = standalone value  (typeof === is a object)
+undefined = error in your code
+
+//objects 
+
+*/
+console.log(3 +3 +4 +5 +6);
+console.log(typeof null);
