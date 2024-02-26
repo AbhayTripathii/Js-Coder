ceed8344bad054640e7640601c01f54237fc2ac5
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-02-26 06:50:49 +0000

    Conversion of Datatypes

---
+++ 01_basics.js/03_conversion.js
@@ -0,0 +1,50 @@
+// let score = 40;
+
+// console.log(typeof score); //use of typeof
+// console.log(typeof(score)); // use of typeof but also use () methods format
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/////////////////////////////////////////////
+
+// let score = null;
+
+// console.log(typeof score); //use of typeof
+// console.log(typeof(score)); // use of typeof but also use () methods format
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/////////////////////////////////////////
+
+// let score = undefined;
+
+// console.log(typeof score); //use of typeof
+// console.log(typeof(score)); // use of typeof but also use () methods format
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//////////////////////////////////////////
+
+// let score = true;
+
+// console.log(typeof score); //use of typeof
+// console.log(typeof(score)); // use of typeof but also use () methods format
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+/*Conversion output NOTES
+40 => number
+null => object
+undefined => undefined
+True => Boolean
+*/
