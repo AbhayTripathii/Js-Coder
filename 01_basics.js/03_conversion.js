89bff1a77225b2aebe8c63147556b022c4783b21
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-02-27 13:26:45 +0000

    Conversation Operation

--- 01_basics.js/03_conversion.js
+++ 01_basics.js/03_conversion.js
@@ -48,3 +48,16 @@ null => object
 undefined => undefined
 True => Boolean
 */
+////////////////////////////////////////////////////////////
+// let value = 3;
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2 + 2 + 2);
+// console.log("1" + 6);
+// console.log(1 + "6");
+// console.log("1" + "6");
+// console.log("1" + 6 + 7); //first he give string then 6  and  7
+// console.log(1 + 6 + "7"); //but inthis he give add the 1 + 6 then give 7
+// console.log(+true); //output 1
+// console.log(true+); //output error
