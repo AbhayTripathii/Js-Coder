fce460d2b2abb86e55537228f3236602d72e4f76
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-02-28 05:55:58 +0000

    comparison

---
+++ 01_basics.js/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 3); // false
+// console.log(2 <= 3); //true
+// console.log(2 >= 3); //false
+// console.log(2 < 3); //true
+// console.log(2 != 3); //true
+
+//Comparison on predictible
+// console.log(null = 0); //Error
+// console.log(null == 0); //false
+// console.log(null != 0); //true
+// console.log(null >= 0); //true
+// console.log(null <= 0); //true
+
+//undefined
+
+console.log(undefined = 0); //0
+console.log(undefined == 0); //false
+console.log(undefined != 0); //true
+console.log(undefined >= 0); //false
+console.log(undefined <= 0); //false
+
