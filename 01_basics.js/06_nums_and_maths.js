fe4085cd1f504f5eb90ab14601401772de54a8b9
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-02-28 11:23:08 +0000

    changes in nums & maths

--- 01_basics.js/06_nums_and_maths.js
+++ 01_basics.js/06_nums_and_maths.js
@@ -1,16 +1,34 @@
-let score = "100";
-console.log(score);
+const score = 400
+// console.log(score);
 
-const balance = new Number(100);
-console.log(balance);
+const balance = new Number(100)
+// console.log(balance);
 
-console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
 
+const otherNumber = 123.8966
 
-const otherNumber = 52.369;
-console.log(otherNumber.toPrecision(1));
-// console.log(score.toString());
-// console.log(score.toFixed(2));
+// console.log(otherNumber.toPrecision(4));
 
-const hundreds = 150000000000;
-console.log(hundreds.toLocaleString('en-IN'));
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
