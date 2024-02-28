4a4ba25b353174a27197d2eaca27951b21348a74
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-02-28 10:16:06 +0000

    changes

---
+++ 01_basics.js/06_nums_and_maths.js
@@ -0,0 +1,16 @@
+let score = "100";
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toFixed(2));
+
+
+const otherNumber = 52.369;
+console.log(otherNumber.toPrecision(1));
+// console.log(score.toString());
+// console.log(score.toFixed(2));
+
+const hundreds = 150000000000;
+console.log(hundreds.toLocaleString('en-IN'));
