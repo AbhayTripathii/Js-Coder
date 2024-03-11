589622ccce06e5ec8500d837e265cd7f1c87724e
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-03-11 15:27:05 +0000

    Date Creating

--- 01_basics.js/07_datesinJS.js
+++ 01_basics.js/07_datesinJS.js
@@ -1,4 +1,15 @@
 //Dates
 
-let myDate = new Date;
-console.log(myDate.toString());
+//let myDate = new Date();
+//console.log(myDate.toString());   //Mon Mar 11 2024 15:18:34 GMT+0000(Coordinated Universal Time)
+//console.log(myDate.toDateString());   //Mon Mar 11 2024
+//console.log(myDate.toISOString());   //2024-03-11T15:18:34.974Z
+//console.log(myDate.toJSON());   //2024-03-11T15:18:34.974Z
+//console.log(myDate.toLocaleDateString());   //3/11/2024
+//console.log(myDate.toLocaleString());   //3/11/2024, 3:20:09 PM
+//console.log(myDate.toLocaleTimeString());   //3:20:09 PM
+
+
+let myCreatedDate = new Date(2075,6,11,10,5)
+console.log(myCreatedDate.toLocalString());
+console.log(myCreatedDate.toLocaleString());
