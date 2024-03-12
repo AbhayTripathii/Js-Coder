dc638b7bfc20b702e331f4c1cc5177336eef1eca
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-03-12 09:20:13 +0000

    array 2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,12 @@
+//Arrays-2
+
+const marvel_heroes = ["thor", "Ironman", "spiderman"]
+const dc_heroes = ["superman", "flash", "batman"]
+
+// marvel_heroes.push(dc_heroes) ///// creating existing array
+
+// console.log(marvel_heroes);
+// console.log(dc_heroes);
+
+const allMerge = marvel_heroes.concat(dc_heroes) //// creating new array
+console.log(allMerge);
