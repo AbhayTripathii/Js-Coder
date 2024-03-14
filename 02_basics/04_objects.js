1ee2e09c0d7124ae9726279a1c9073a5eba46af4
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-03-14 16:38:09 +0000

    object part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,81 @@
+// const tinderUser = new Object()   ------ singelton object
+const tinderUser = {} // non-singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "sammy"
+tinderUser.isLoggedIn = false
+
+// .console.log(tinderUser);
+
+const regularUser = {
+    email: "any@gmail.com",
+    fullName: {
+        userFullname: {
+            firstName: "Abhay",
+            lastName: "Tripathi"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.userFullname.firstName);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({},obj1, obj2) // merge in objects like this
+
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
