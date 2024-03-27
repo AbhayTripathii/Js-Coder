970ffd5f26f8612f919da26573465e81151728d6
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-03-27 09:29:51 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,12 @@
+// if 
+const isUserLoggedIn = true
+const temperature = 30
+
+if ( temperature <= 40){
+    console.log("Confort zone");
+} else {
+console.log("temperature is grator than 50")
+}
+
+
+// <, >, <=, >=, ==, !=, ===, !==
