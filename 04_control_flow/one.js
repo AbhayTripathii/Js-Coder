3d788025d9d2e7a5244eeedfacf81fea45668578
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-03-27 10:18:36 +0000

    constrol flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -3,10 +3,49 @@ const isUserLoggedIn = true
 const temperature = 30
 
 if ( temperature <= 40){
-    console.log("Confort zone");
+    // console.log("Confort zone");
 } else {
-console.log("temperature is grator than 50")
+// console.log("temperature is grator than 50")
 }
 
+// console.log("executed")
 
 // <, >, <=, >=, ==, !=, ===, !==
+
+
+// const score = 200
+
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+// if (balance > 500) console.log("test"), console.log("test2");
+
+//  if (balance < 500){
+//     console.log("less than 500")
+//  } else if(balance < 750) {
+//     console.log("less than 750")
+//  } else if(balance < 900) {
+//     console.log("less than 900")
+//  } else {
+//     console.log("less than 1200")
+//  }
+
+
+
+const userLoggedIn = true
+const debitCard = true
+const LoggedInFromGoogle = false
+const LoggedInFromEmail = true
+
+if (userLoggedIn && debitCard){   // && == if all statement are true i am execute
+    console.log("Allow to buy course")
+}
+
+if (LoggedInFromEmail || LoggedInFromGoogle){  // || any statement are true i am execute
+    console.log("User Logged in")
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "feb"
+
+switch (month) {
+    case "jan":
+        console.log("January");     
+        break;
+    case "feb":
+        console.log("Feb");     
+        break;
+    case "march":
+        console.log("March");     
+        break;
+    case "april":
+        console.log("April");     
+        break;
+    default:
+        console.log("Default case match")
+        break;
+}
