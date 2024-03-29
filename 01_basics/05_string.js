c77911daffc410df0dfccf59d1b8359266bf7dc5
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-03-30 00:20:37 +0530

    make notes

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -11,8 +11,8 @@
 // // console.log(gameName.__proto__);
 
 
-// // console.log(gameName.length);
-// // console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
 // console.log(gameName.charAt(2));
 // console.log(gameName.indexOf('t'));
 
