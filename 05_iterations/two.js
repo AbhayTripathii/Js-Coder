a3a19c8245081989c79076d078448c06b8c60530
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-03-30 15:39:43 +0000

    while, Do while loops end

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+//While loop
+
+// let index = 0
+
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`)
+//     index = index + 2        
+// }
+
+
+let myArray = ["cap","iron","Thor"]
+
+let arr = 0 // for increment decrement
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr++    
+}
+
+
+// Do While loop
+let score = 1
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score <= 10);
