e9bd40548d1163966b6c5bd3acf73df876284f3f
Abhay Tripathi <134273693+AbhayTripathii@users.noreply.github.com>
2024-05-22 12:16:57 +0530

    Add files via upload

---
+++ 01_basics/01_date.js
@@ -0,0 +1,27 @@
+// Return the number of milliseconds elapsed since January 1, 1970, 00:00:00 UTC.
+
+// let comNewDate = Date.now();
+// console.log(comNewDate);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Return the time difference in milliseconds from, January 1, 1970, till the date we provide.
+
+// let returnDate = "September 11, 2005 3:25 AM";
+
+// let compileDate = Date.parse(returnDate);
+// console.log(compileDate);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// let A = new Date("November 11, 2005 03:25:50");
+
+// let B = A.getDate();
+// console.log(B);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+let A = new Date("October 15, 1996 05:35:32");
+
+let B = A.getDay();
+console.log(B);
